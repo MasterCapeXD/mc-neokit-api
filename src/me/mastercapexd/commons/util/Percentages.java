69af3c87c74c75f7dc8e96f7ae44dae30693db8e
MasterCapeXD <mastercapexd@gmail.com>
2020-04-21 14:49:21 +0300

    Percentages update

--- src/me/mastercapexd/commons/util/Percentages.java
+++ src/me/mastercapexd/commons/util/Percentages.java
@@ -1,26 +1,20 @@
 package me.mastercapexd.commons.util;
 
+import java.util.Collection;
+
 public class Percentages {
 
 	public static <T> PercentageSet<T> newHashPercentageSet() {
 		return new HashPercentageSet<>();
 	}
 	
-	private Percentages() {
-		throw new UnsupportedOperationException("This class cannot be instantiated!");
+	public static <T> PercentageSet<T> newHashPercentageSet(Collection<T> collection) {
+		PercentageSet<T> set = new HashPercentageSet<>();
+		set.addAll(collection);
+		return set;
 	}
 	
-	public static void main(String[] args) {
-		
-		PercentageSet<String> percentageSet = Percentages.newHashPercentageSet();
-		percentageSet.put("Entry1", 30);
-		percentageSet.put("Entry2", 9);
-		percentageSet.put("Entry3", 9);
-		percentageSet.put("Entry4", 22);
-		percentageSet.put("Entry5", 50);
-		
-		for (int i = 0; i <= 50; i++)
-			System.out.println(percentageSet.get());
-		
+	private Percentages() {
+		throw new UnsupportedOperationException("This class cannot be instantiated!");
 	}
 }
